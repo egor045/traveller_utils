a967ccf6f95852faff9afcfe22e736945863853f
Arthur Green <arthur@winston.phraction.org>
2020-08-13 15:20:48 +0100

    Remove map, hex (see BasePlanet change) Add trade classifications

--- tests/ct/test_ct_planet.py
+++ tests/ct/test_ct_planet.py
@@ -26,7 +26,95 @@ class TestCTPlanet(unittest.TestCase):
         self.log.setLevel(logging.DEBUG)
     
     def test_create(self):
-        ''' Planet test create'''
+        ''' CT Planet test create'''
         p = Planet()
         self.assertIsInstance(p, Planet)
     
+    def test_trade_classification(self):
+        ''' CT Planet trade classificaions'''
+
+        # Ag
+        for atm in "456789":
+            for hyd in "45678":
+                for pop in "567":
+                    upp = "X5{}{}{}00-0".format(atm, hyd, pop)
+                    p1977 = Planet(upp=upp, mode="1977")
+                    p1981 = Planet(upp=upp, mode="1981")
+                    self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+                    self.assertTrue("Ag" in p1977.trade_classifications)
+                    self.assertTrue("Ag" in p1981.trade_classifications)
+        # Na
+        for atm in "0123":
+            for hyd in "0123":
+                for pop in "6789A":
+                    upp = "X5{}{}{}00-0".format(atm, hyd, pop)
+                    p1977 = Planet(upp=upp, mode="1977")
+                    p1981 = Planet(upp=upp, mode="1981")
+                    self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+                    self.assertTrue("Na" in p1977.trade_classifications)
+                    self.assertTrue("Na" in p1981.trade_classifications)
+        # In
+        for atm in "012479":
+            for pop in "9A":
+                upp = "X5{}0{}00-0".format(atm, pop)
+                p1977 = Planet(upp=upp, mode="1977")
+                p1981 = Planet(upp=upp, mode="1981")
+                self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+                self.assertTrue("In" in p1977.trade_classifications)
+                self.assertTrue("In" in p1981.trade_classifications)
+        # Ni
+        for pop in "0123456":
+            upp = "X550{}00-0".format(pop)
+            p1977 = Planet(upp=upp, mode="1977")
+            p1981 = Planet(upp=upp, mode="1981")
+            self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+            self.assertTrue("Ni" in p1977.trade_classifications)
+            self.assertTrue("Ni" in p1981.trade_classifications)
+        # Ri
+        for gov in "456789":
+            for atm in "68":
+                for pop in "678":
+                    upp = "X5{}0{}{}0-0".format(atm, pop, gov)
+                    p1977 = Planet(upp=upp, mode="1977")
+                    p1981 = Planet(upp=upp, mode="1981")
+                    self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+                    self.assertTrue("Ri" in p1977.trade_classifications)
+                    self.assertTrue("Ri" in p1981.trade_classifications)
+        # Po
+        for atm in "2345":
+            for hyd in "0123":
+                upp = "X5{}{}500-0".format(atm, hyd)
+                p1977 = Planet(upp=upp, mode="1977")
+                p1981 = Planet(upp=upp, mode="1981")
+                self.log.debug("upp: %s p1977 TCs: %s p1981 TCs: %s", upp, p1977.trade_classifications, p1981.trade_classifications)
+                self.assertTrue("Po" in p1977.trade_classifications)
+                self.assertTrue("Po" in p1981.trade_classifications)
+        # De
+        p1981 = Planet(upp="X510000-0")
+        self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+        self.assertTrue("De" in p1981.trade_classifications)
+        p1981 = Planet(upp="X000000-0")
+        self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+        self.assertTrue("De" not in p1981.trade_classifications)
+        # Va
+        p1981 = Planet(upp="X500000-0")
+        self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+        self.assertTrue("Va" in p1981.trade_classifications)
+        p1981 = Planet(upp="X000000-0")
+        self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+        self.assertTrue("Va" not in p1981.trade_classifications)
+        # As
+        p1981 = Planet(upp="X000000-0")
+        self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+        self.assertTrue("As" in p1981.trade_classifications)
+        # Ic
+        for hyd in "123456789A":
+            for atm in "01":
+                p1981 = Planet(upp="X5{}{}000-0".format(atm, hyd))
+                self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+                self.assertTrue("Ic" in p1981.trade_classifications)
+        for hyd in "123456789A":
+            for atm in "3456789ABC":
+                p1981 = Planet(upp="X5{}{}000-0".format(atm, hyd))
+                self.log.debug("upp: %s p1981 TCs: %s", upp, p1981.trade_classifications)
+                self.assertTrue("Ic" not in p1981.trade_classifications)
--- traveller_utils/ct/planet.py
+++ traveller_utils/ct/planet.py
@@ -4,14 +4,15 @@ from ehex import ehex
 
 from traveller_utils.planet import BasePlanet
 from traveller_utils.util import Die
+from traveller_utils.ct.trade_classifications import TradeClassification
 
 D6 = Die(6)
 
 class Planet(BasePlanet):
     ''' CT planet extends BasePlanet'''
 
-    def __init__(self, upp: str="", name: str="", map: str="", hex: str="", mode: str="1981"):
-        super().__init__(upp=upp, name=name, map=map, hex=hex)
+    def __init__(self, upp: str="", name: str="", mode: str="1981"):
+        super().__init__(upp=upp, name=name)
         try:
             assert str(mode) in ["1977", "1981"]
         except AssertionError:
@@ -19,6 +20,7 @@ class Planet(BasePlanet):
         self.__mode = str(mode)
         if upp == "":
             self.generate()
+        self.trade_classification()
     
     def generate(self):
         ''' Generate UPP'''
@@ -49,8 +51,20 @@ class Planet(BasePlanet):
             atmosphere = ehex(r)
         
         # Hydrographics
-        dm = int(size) - 7
-        if size in [0, 1]:
+        ''' 1977 version: 
+            - size = 0 or 1 => hydro = 0
+            - modifier based on size
+            1981 version: 
+            - size = 0 => hydro = 0
+            - modifier based on atmosphere
+        '''
+        if self.__mode == "1977":
+            dm = int(size) - 7
+        else:
+            dm = int(atmosphere) - 7
+        if self.__mode == "1977" and size in [0, 1]:
+            hydrographics = 0
+        elif self.__mode == "1981" and size == 0:
             hydrographics = 0
         else:
             if atmosphere <= 1 or atmosphere >= 10:
@@ -113,3 +127,41 @@ class Planet(BasePlanet):
             tech_level
         )
         self.import_upp(upp)
+
+    def trade_classification(self):
+        ''' Determine trade classifications for planet'''
+        # Ag
+        if (4 <= self.atmosphere <= 9) and (5 <= self.population <= 7) and (4 <= self.hydrographics <= 8):
+            self.trade_classifications.append("Ag")
+        # Na
+        if self.atmosphere <= 3 and self.hydrographics <= 3 and self.population >= 6:
+            self.trade_classifications.append("Na")
+        # In
+        if self.population >= 9 and self.atmosphere in [0, 1, 2, 4, 7, 9]:
+            self.trade_classifications.append("In")
+        # Ni
+        if self.population <= 6:
+            self.trade_classifications.append("Ni")
+        # Ri
+        if (4 <= self.government <= 9) and (self.atmosphere in [6, 8]) and (6 <= self.population <= 8):
+            self.trade_classifications.append("Ri")
+        # Po
+        if (2 <= self.atmosphere <= 5) and self.hydrographics <= 3:
+            self.trade_classifications.append("Po")
+        
+        '''
+        1981 extras:
+            - Desert (De)
+            - Vacuum (Va)
+            - Asteroid (As)
+            - Ice-capped (Ic)
+        '''
+        if self.__mode == "1981":
+            if self.hydrographics == 0 and self.size != 0:
+                self.trade_classifications.append("De")
+            if self.atmosphere == 0 and self.size != 0:
+                self.trade_classifications.append("Va")
+            if self.size == 0:
+                self.trade_classifications.append("As")
+            if self.atmosphere <= 1 and self.hydrographics >= 1:
+                self.trade_classifications.append("Ic")
